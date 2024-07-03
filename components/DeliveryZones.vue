33f79342f58f7a98394f9f1151294a49f4462591
abdulbasetbappy <abdulbasetbappy@hotmail.com>
2024-07-04 01:51:14 +0600

    Delivery Zones All Filed Added

--- components/DeliveryZones.vue
+++ components/DeliveryZones.vue
@@ -3,7 +3,7 @@
     <!-- Header -->
     <div class="flex flex-row justify-between items-center">
       <div class="flex flex-row gap-3">
-            <h1 class="text-xl font-medium">Add Locations</h1>
+            <h1 class="text-xl font-medium">Add Delivery Zones</h1>
             <Button
                 class="youtube px-4 py-1 bg-gray-200"
                 aria-label="Youtube"
@@ -19,38 +19,9 @@
     </div>
 
     <!-- Dynamic Form Fields -->
-     <Fieldset :legend="`Location ${index + 1}`" v-for="(field, index) in fields" :key="index" class="mt-4 border border-gray-300 p-4 pt-0 mr-16">
+     <Fieldset :legend="`Zone ${index + 1}`" v-for="(field, index) in fields" :key="index" class="mt-4 border border-gray-300 p-4 pt-0 mr-16">
         <div>
-        <label class="block text-sm font-medium mb-1">Name</label>
-        <input
-            v-model="field.name"
-            placeholder="Name"
-            class="block mb-2 border border-gray-300 w-[100%] outline-none py-1 px-2 rounded-md focus:border-indigo-500"
-        />
-        <div class="grid grid-cols-2 gap-4">
-            <div>
-            <label class="block text-sm font-medium mb-1">Address</label>
-            <Textarea
-                v-model="field.address"
-                placeholder="Address"
-                class="block mb-2 h-[100px] border border-gray-300 w-[100%] outline-none py-1 px-2 rounded-md focus:border-indigo-500"
-            />
-            </div>
-            <div>
-            <label class="block text-sm font-medium mb-1">Latitude</label>
-            <input
-                v-model="field.latitude"
-                placeholder="Latitude"
-                class="block mb-2 border border-gray-300 w-[100%] outline-none py-1 px-2 rounded-md focus:border-indigo-500"
-            />
-            <label class="block text-sm font-medium mb-1">Longitude</label>
-            <input
-                v-model="field.longitude"
-                placeholder="Longitude"
-                class="block mb-2 border border-gray-300 w-[100%] outline-none py-1 px-2 rounded-md focus:border-indigo-500"
-            />
-            </div>
-        </div>
+          <!--1st Row-->
         <div class="grid grid-cols-3 gap-4">
             <div>
             <label class="block text-sm font-medium mb-1">City</label>
@@ -77,24 +48,65 @@
             />
             </div>
         </div>
-        <div class="grid grid-cols-2 gap-4">
-            <div>
-            <label class="block text-sm font-medium mb-1">Phone</label>
+        <!--2nd Row-->
+        <div class="grid grid-cols-4 gap-4 mb-4">
+          <div>
+            <label class="block text-sm font-medium mb-1">Latitude</label>
             <input
-                v-model="field.phone"
-                placeholder="Phone"
+                v-model="field.latitude"
+                placeholder="Latitude"
+                class="block mb-2 border border-gray-300 w-[100%] outline-none py-1 px-2 rounded-md focus:border-indigo-500"
+            />
+          </div>
+          <div>
+              <label class="block text-sm font-medium mb-1">Longitude</label>
+            <input
+                v-model="field.longitude"
+                placeholder="Longitude"
                 class="block mb-2 border border-gray-300 w-[100%] outline-none py-1 px-2 rounded-md focus:border-indigo-500"
             />
             </div>
-            <div>
-            <label class="block text-sm font-medium mb-1">Email</label>
+          <div>
+              <label class="block text-sm font-medium mb-1">Min. Cart</label>
             <input
-                v-model="field.email"
-                placeholder="Email"
+                v-model="field.minCart"
+                placeholder="Min. Cart Amount"
                 class="block mb-2 border border-gray-300 w-[100%] outline-none py-1 px-2 rounded-md focus:border-indigo-500"
             />
+          </div>
+              <div class="h-5">
+                <label class="block text-sm font-medium mb-1">Managed By</label>
+                  <Dropdown v-model="field.managedBy" :options="zoneHandler" optionLabel="name" placeholder="Managed By" class=" mb-2 border border-gray-300 w-[100%] outline-none rounded-md focus:border-indigo-500" />
             </div>
+          </div>
         </div>
+        <!--3rd Row-->
+        <label class="text-md font-medium">Delivery Charges</label>
+        <div class="grid grid-cols-3 gap-4">
+          <div>
+            <label class="block text-sm font-medium mb-1">Min</label>
+            <input
+                v-model="field.minDelivery"
+                placeholder="Min Delivery"
+                class="block mb-2 border border-gray-300 w-[100%] outline-none py-1 px-2 rounded-md focus:border-indigo-500"
+            />
+            </div>
+            <div>
+            <label class="block text-sm font-medium mb-1">Max</label>
+            <input
+                v-model="field.maxDelivery"
+                placeholder="Max Delivery"
+                class="block mb-2 border border-gray-300 w-[100%] outline-none py-1 px-2 rounded-md focus:border-indigo-500"
+            />
+            </div>
+            <div>
+              <label class="block text-sm font-medium mb-1">Charge</label>
+              <input
+                v-model="field.charge"
+                placeholder="Charge"
+                class="block mb-2 border border-gray-300 w-[100%] outline-none py-1 px-2 rounded-md focus:border-indigo-500"
+              />
+            </div>
         </div>
     </Fieldset>
     <!-- Display Collected Data -->
@@ -108,18 +120,27 @@
 <script setup>
 import { ref, reactive } from "vue";
 import Button from "primevue/button";
+import Dropdown from 'primevue/dropdown';
+
+
 
 const fields = ref([{
-  name: '',
-  address: '',
+  minCart: '',
   city: '',
   zip: '',
   state: '',
-  phone: '',
-  email: '',
   latitude: '',
-  longitude: ''
+  longitude: '',
+  minDelivery: '',
+  maxDelivery: '',
+  charge: '',
+  managedBy: {},
 }]); // Reactive state for form fields
+const zoneHandler = reactive([
+  {name:'Admin',code:'01'},
+  {name:'Admin',code:'01'},
+  {name:'Admin',code:'01'},
+])
 const submitted = ref(false);
 
 // Function to add a new field
@@ -143,6 +164,6 @@ const handleSubmit = () => {
 };
 </script>
 
-<style scoped>
+<style >
 /* Add your custom styles here */
 </style>
